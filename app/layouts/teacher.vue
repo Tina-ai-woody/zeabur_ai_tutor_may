900df7b17465d26442d1e159f70f6ce8dbb682c2
woody <johannes8873@gmail.com>
2025-11-25 20:51:47 +0800

    feat: small change on teacher's layout

--- app/layouts/teacher.vue
+++ app/layouts/teacher.vue
@@ -18,18 +18,18 @@ const handleLogout = async () => {
       <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
           <li>
-<<<<<<< HEAD
             <NuxtLink class="btn btn-ghost" to="/teacher/problems"
               >Manage Problems</NuxtLink
-=======
-            <NuxtLink class="btn btn-ghost" to="/teacher/classrooms"
-              >Classrooms</NuxtLink
->>>>>>> classroom
             >
           </li>
           <li>
             <NuxtLink class="btn btn-ghost" to="/teacher/problems/create"
               >Create Problem</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink class="btn btn-ghost" to="/teacher/classrooms"
+              >Classrooms</NuxtLink
             >
           </li>
           <li>
