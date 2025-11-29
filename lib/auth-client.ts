2a88653f55a780613f279c1ed24bbd6f5334fdc5
woody <johannes8873@gmail.com>
2025-11-29 22:38:44 +0800

    try 2

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,12 +1,15 @@
 import { createAuthClient } from "better-auth/vue";
 import { adminClient } from "better-auth/client/plugins";
 
+const baseURL = process.client
+  ? window.location.origin
+  : process.env.NUXT_PUBLIC_SITE_URL;
+
 export const authClient = createAuthClient({
   // baseURL: import.meta.env.PROD
   //   ? "https://studywithwoody.site"
   //   : "http://localhost:3000",
-  baseURL:
-    (useRuntimeConfig().public.baseURL as string) || window.location.origin,
+  baseURL,
   plugins: [adminClient()],
 });
 
